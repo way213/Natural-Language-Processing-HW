ed48b84a0579d9e2eca190c112da5fc7cdfbb793
way213 <way213@nyu.edu>
2024-02-14 14:29:08 -0500

    create merge method
    method merges two files together

--- hw3/test.py
+++ hw3/test.py
@@ -1,18 +1,23 @@
 keyFileName = 'data/WSJ_24.pos'
-responseFileName = 'results_WSJ_24.pos'
+responseFileName = 'data/WSJ_02-21.pos'
 
-def score (keyFileName, responseFileName):
-	keyFile = open(keyFileName, 'r')
-	key = keyFile.readlines()
-	responseFile = open(responseFileName, 'r')
-	response = responseFile.readlines()
-	print(len(key))
-	print(len(response))
-	if len(key) != len(response):
-		print("length mismatch between key and submitted file")
-		exit()
-		
+# Assuming the score module is correctly imported and used elsewhere
 from data.score import score
 
-score (keyFileName, responseFileName)
 
+def merge_data(file1, file2):
+	# Name of the newly merged file
+	new_file = 'merged_training_set.pos'
+
+	# Append content from the first file
+	with open(responseFileName, 'r', encoding='utf8') as csv_file:
+		with open(new_file, "w", encoding='utf8') as merged_file:
+			for line in csv_file:
+				merged_file.write(line)
+
+	# Append content from the second file
+	with open(keyFileName, 'r', encoding='utf8') as payload:
+		with open(new_file, "a", encoding='utf8') as merged_file:  # Use append mode here
+			for line in payload:
+				merged_file.write(line)
+	return new_file
